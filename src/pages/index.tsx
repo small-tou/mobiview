8fb6d12583db6d3f2d6c2b13cf6627e5820407ab
yootou <xinyu198736@gmail.com>
2023-04-07 20:18:28 +0800

    Update styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import { useEffect, useState } from 'react';
-
+import { Text } from '@nextui-org/react';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
@@ -35,68 +35,119 @@ export default function Home() {
   const [newURL, setNewURL] = useState('');
 
   return (
-    <main
-      className="flex flex-col items-center justify-center  "
+    <div
+      className="flex flex-column items-center justify-center main-body"
       style={{
-        background: '#eee',
+        width: '100vw',
+        height: '100vh',
+        background: '#F7F7F7',
       }}
     >
-      <div className="flex flex-row items-center justify-center p-5 gap-10">
-        {Object.keys(mobileScreenSizes).map((key) => (
-          <a
-            key={key}
-            style={{
-              fontSize: '14px',
-              cursor: 'pointer',
-            }}
-            className={`${
-              screenSize === key ? 'text-blue-500 text-bold' : 'text-black-500'
-            } hover:text-blue-500`}
-            onClick={() => {
-              activeScreenSize(key);
-            }}
-          >
-            {key}
-          </a>
-        ))}
-      </div>
-      {url && (
-        <iframe
-          src={url}
-          style={{
-            border: 'none',
-            borderRadius: '15px',
-            background: '#fff',
-            width,
-            height,
-            boxShadow: '0 0 10px rgba(0,0,0,0.1)',
-            overflow: 'hidden',
-          }}
-        ></iframe>
-      )}
-
-      <div className="flex flex-row items-center justify-center p-10 gap-5">
-        <input
-          type="text"
-          className="border-2 border-gray-300 p-2"
-          onChange={(e) => {
-            setNewURL(e.target.value);
-          }}
-          style={{
-            width: '300px',
-            borderRadius: '8px',
-          }}
-          placeholder="Enter URL"
-        />
-        <button
-          className="bg-blue-500 text-white p-2 rounded-md"
-          onClick={() => {
-            window.location.href = `/?url=${encodeURIComponent(newURL)}`;
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minWidth: '500px',
+          maxWidth: '1000px',
+        }}
+      >
+        <Text
+          h1
+          size={40}
+          css={{
+            textGradient: '45deg, $purple600 0%, $pink600 100%',
+            letterSpacing: '0.03em',
           }}
         >
-          Go to URL
-        </button>
+          MobiView.Pro
+        </Text>
+
+        <div
+          className="flex flex-row items-center justify-center p-5 "
+          style={{
+            width: '460px',
+            gap: '15px',
+            flexWrap: 'wrap',
+          }}
+        >
+          {Object.keys(mobileScreenSizes).map((key) => (
+            <a
+              key={key}
+              style={{
+                fontSize: '18px',
+                cursor: 'pointer',
+                width: '200px',
+                height: '90px',
+                borderRadius: '10px',
+                textAlign: 'center',
+                display: 'flex',
+                alignItems: 'center',
+                color: '#fff',
+                justifyContent: 'center',
+              }}
+              className={`${
+                screenSize === key ? 'bg-blue-500 text-bold' : 'bg-gray-500'
+              } hover:bg-blue-500`}
+              onClick={() => {
+                activeScreenSize(key);
+              }}
+            >
+              {key}
+            </a>
+          ))}
+        </div>
+        <div className="flex flex-row items-center justify-center p-10 gap-5">
+          <input
+            type="text"
+            className="border-2 border-gray-300 p-2"
+            onChange={(e) => {
+              setNewURL(e.target.value);
+            }}
+            style={{
+              width: '280px',
+              borderRadius: '8px',
+              padding: '10px 20px',
+            }}
+            placeholder="Enter URL"
+          />
+          <button
+            className="bg-blue-500 text-white p-2 rounded-md"
+            onClick={() => {
+              window.location.href = `/?url=${encodeURIComponent(newURL)}`;
+            }}
+            style={{
+              padding: '10px 20px',
+            }}
+          >
+            Go to URL
+          </button>
+        </div>
       </div>
-    </main>
+      <div
+        style={{
+          flex: 1,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {url && (
+          <iframe
+            src={url}
+            style={{
+              border: 'none',
+              borderRadius: '15px',
+              background: '#fff',
+              width,
+              height,
+              boxShadow: '0 0 10px rgba(0,0,0,0.1)',
+              overflow: 'hidden',
+            }}
+          ></iframe>
+        )}
+      </div>
+    </div>
   );
 }
