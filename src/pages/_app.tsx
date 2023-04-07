55b41aad0ebb76218bacdf087e2dc64b2b877a6c
yootou <xinyu198736@gmail.com>
2023-04-07 21:49:29 +0800

    Update style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-
+import toast, { Toaster } from 'react-hot-toast';
 import { NextUIProvider } from '@nextui-org/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <Component {...pageProps} />
+      <Toaster />
     </NextUIProvider>
   );
 }
